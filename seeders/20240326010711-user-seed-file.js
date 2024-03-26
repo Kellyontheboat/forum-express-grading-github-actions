34fc5c915c4538a8141cfd406b159718d6a52878
Kellyontheboat <sha22595911sha49@gmail.com>
2024-03-26 11:19:59 +0800

    feat: add seed data

---
+++ seeders/20240326010711-user-seed-file.js
@@ -0,0 +1,32 @@
+'use strict';
+const bcrypt = require('bcryptjs') // 引入 bcryptjs 套件
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [{ // 一次新增三筆資料
+      email: 'root@example.com',
+      password: await bcrypt.hash('22595911123', 10),
+      is_admin: true,
+      name: 'root',
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      email: 'user1@example.com',
+      password: await bcrypt.hash('22595911456', 10),
+      is_admin: false,
+      name: 'user1',
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      email: 'user2@example.com',
+      password: await bcrypt.hash('22595911789', 10),
+      is_admin: false,
+      name: 'user2',
+      created_at: new Date(),
+      updated_at: new Date()
+    }], {})
+  },
+  down: async (queryInterface, Sequelize) => { // 清空資料表中所有資料
+    await queryInterface.bulkDelete('Users', {})
+  }
+}
