ca9ab466dee6abf1ff16f89c11cba351657f6b02
Kellyontheboat <sha22595911sha49@gmail.com>
2024-03-28 21:00:22 +0800

    add users data

--- seeders/20240326010711-user-seed-file.js
+++ seeders/20240326010711-user-seed-file.js
@@ -5,21 +5,21 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Users', [{ // 一次新增三筆資料
       email: 'root@example.com',
-      password: await bcrypt.hash('22595911123', 10),
+      password: await bcrypt.hash('12345678', 10),
       is_admin: true,
       name: 'root',
       created_at: new Date(),
       updated_at: new Date()
     }, {
       email: 'user1@example.com',
-      password: await bcrypt.hash('22595911456', 10),
+      password: await bcrypt.hash('12345678', 10),
       is_admin: false,
       name: 'user1',
       created_at: new Date(),
       updated_at: new Date()
     }, {
       email: 'user2@example.com',
-      password: await bcrypt.hash('22595911789', 10),
+      password: await bcrypt.hash('12345678', 10),
       is_admin: false,
       name: 'user2',
       created_at: new Date(),
