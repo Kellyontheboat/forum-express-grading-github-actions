a30eb21b37bef37393787bd82b2af55d0caa9eda
Kellyontheboat <sha22595911sha49@gmail.com>
2024-04-10 17:09:34 +0800

    feat: show comments on restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,12 +1,14 @@
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, User, Comment } = require('../models')
 
 const restaurantController = {
 
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category,
-      // 若沒有加入 nest: true &raw: true，則會回傳 Sequelize 物件,後面需要用restaurant.toJSON()轉換成JSON格式
+      include: [
+        Category,
+        { model: Comment, include: User }
+      ],
       nest: true
     }).then(restaurant => {
       if (!restaurant) throw new Error('Restaurant not found!')
